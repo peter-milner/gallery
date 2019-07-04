0168aed445fddb37c52efca49bab707c5118da57
peter <peter.milner@ryerson.ca>
2019-07-03 22:18:01 -0400

    switch index route to render template

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 # pylint: disable=C0103,C0111
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
