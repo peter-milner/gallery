06e63e5127de585bac500617c6469271f2b12b74
peter <peter.milner@ryerson.ca>
2019-07-05 19:12:17 -0400

    add photos query to base component

--- src/static/js/components/base.js
+++ src/static/js/components/base.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+
+export const query = gql`
+    query ($page: Int!) {
+        photos(page: $page) {
+            currentPage
+            totalPages
+            photos {
+                url
+            }
+        }
+    }
+`
 
 export default function Base () {
+    const [page, setPage] = useState(1)
+
     return (
-        <div className='container'>
-            <h1 className='title'>What's popular today</h1>
-        </div>
-    )
+        <Query
+            query={query}
+            variables={{page: page}}
+        >
+            {({ data }) => {
+                console.log(data)
+                return (
+                    <div className='container'>
+                        <h1 className='title'>What's popular today</h1>
+                    </div>
+                )
+            }}
+        </Query>
+    )  
 }
